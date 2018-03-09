fa0c17b9ee6d99ea006a56094fe6e2aa5056f5c7
¯\_(ツ)_/¯ <6903543+NoParamedic@users.noreply.github.com>
2018-03-09 04:25:13 -0800

    Meme Express Choo Choo
    Meme Express Choo Choo

--- src/test/kotlin/pubg/radar/OfflineDemo.kt
+++ src/test/kotlin/pubg/radar/OfflineDemo.kt
@@ -4,6 +4,7 @@ import pubg.radar.sniffer.Sniffer.Companion.sniffLocationOffline
 import pubg.radar.ui.GLMap
 
 fun main(args: Array<String>) {
-  sniffLocationOffline()
-  GLMap().show()
+    Args = args
+    sniffLocationOffline()
+    GLMap().show()
 }
